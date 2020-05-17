78c13c1bb860ce647e0b8fe6ae156c74314ceefd
ortal michael <ortal15michael@gmali.com>
2020-05-17 05:02:29 +0300

    Update User Manager class

--- src/com/business/UserManager.java
+++ src/com/business/UserManager.java
@@ -1,13 +1,68 @@
 package com.business;
+import com.persistent.PermissionLevel;
+import com.persistent.User;
+import com.persistent.Team;
+import java.io.File;
+import java.util.HashMap;
+
 
 public class UserManager {
 
+    private File usersFile;
+    private User loggedInUser;
+    private HashMap <String, User> users;
 
-
-
-    public Boolean isUserExist(String username)
-    {
-        return true;
+    public UserManager() {
+        this.users = new HashMap<>();
     }
 
+    public void readUsersFile(){}
+
+    public void addUser(String username, String password, PermissionLevel permission, Team team){
+        //TODO: TRY-addUser
+        if (isActionPermitted())
+            if(!(isUserExist(username))){
+                User newUser=new User(username,password,permission,team);
+                users.put(username,newUser);
+             }
+    }
+
+    public Boolean isUserExist(String username) {
+        return users.containsKey(username);
+    }
+
+    public void removeUser(User user){
+        //TODO: TRY - removeUser
+        //TODO: the object removed?
+        if (isActionPermitted())
+            users.remove(user.getUserName(),user);
+
+    }
+
+    public void updateUserPermission(User user, PermissionLevel newPermission){
+        if (isActionPermitted())
+            user.setPermissionLevel(newPermission);
+    }
+
+    public void updateUserTeam(User user,Team newTeam){
+        if (isActionPermitted())
+            user.setTeam(newTeam);
+    }
+
+    public Boolean login(String username,String password){
+        if (isUserExist(username))
+            if ((users.get(username).getPassword().equals(password))){
+                this.loggedInUser = users.get(username);
+                return true;
+            }
+         return false;
+    }
+
+    public Boolean isActionPermitted(){
+        return (this.loggedInUser.getPermissionLevel().equals(PermissionLevel.manager) || this.loggedInUser.getPermissionLevel().equals(PermissionLevel.admin));
+    }
+
+    public void updateUsersFile(){
+
+    }
 }
