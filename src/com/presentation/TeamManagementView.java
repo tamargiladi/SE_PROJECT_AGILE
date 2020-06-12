6de1be46d75d790e12945da5583472efb410bdd6
yuval levi <youval76@gmail.com>
2020-06-12 23:12:26 +0300

    Moving team update functionality from TMView to TM;

--- src/com/presentation/TeamManagementView.java
+++ src/com/presentation/TeamManagementView.java
@@ -548,7 +548,7 @@ public class TeamManagementView extends JFrame {
                 generateTable();
             }
         });
-
+        this.teamsScreenViewPanel.add(comboTeamView);
     }
 
     public List<String> getUsersListByCombo() {
