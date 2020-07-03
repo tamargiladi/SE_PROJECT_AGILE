026cfb293c39577568080dfe56dd70a02b8b9ef9
ortal michael <ortal15michael@gmali.com>
2020-07-03 07:59:11 +0300

    Add UserManagerTest

--- src/com/business/UserManager.java
+++ src/com/business/UserManager.java
@@ -1,7 +1,6 @@
 package com.business;
 
 import com.persistent.*;
-import com.presentation.LoginView;
 import com.presentation.MainUserInterface;
 
 import java.io.*;
@@ -16,7 +15,7 @@ public class UserManager {
 
     private static UserManager UserManagerInstance; //Singleton instance
 
-    private static String fileAddress = "src/com/data/usersFile.ser";
+    private static String userFileAddress = "src/com/data/usersFile.ser";
     public User loggedInUser;
     public HashMap<String, User> users;
 
@@ -35,9 +34,9 @@ public class UserManager {
             /*An admin user must exist in the system to ensure
               that there is at least one user with all permissions */
             if(!(isUserExist("admin"))){
-                /*for adding a user must be a team
+                /*for adding a user must be a team,
                   create default team */
-                LoginView.teamManager.addTeam("default");
+                TeamManager.getInstance().addTeam("default");
                 addUser("admin","admin", User.PermissionLevel.admin,"default");
             }
 
@@ -54,16 +53,23 @@ public class UserManager {
                   3- user not created
          */
     {
-        //create user admin when the system run in the first time
-        if (loggedInUser==null && username.equals("admin"))
-            if (addNewUser(username,password, permission,teamName))
+        //create user admin when the system run in the first time (login not done yet)
+        if (loggedInUser==null && username.equals("admin")) {
+            if (addNewUser(username, password, permission, teamName)) {
+                System.out.println("Admin user created\n");
                 return 1;
-
+            }
+            System.out.println(username+" user not created\n");
+            return 3;
+        }
         //add new user after login
         if (isActionPermitted()) {
-            if (addNewUser(username, password, permission, teamName))
+            if (addNewUser(username, password, permission, teamName)){
+                System.out.println(username+" user created\n");
                 return 1;
-            else  return 3;
+            }
+            System.out.println(username+" user not created\n");
+            return 3;
         }
         // Action no permitted
         System.out.println("Action no permitted\n");
@@ -75,11 +81,14 @@ public class UserManager {
     {
         //check if user not exist in system
         if (!(isUserExist(username))) {
+            //check if all inputs are valid
+            if ((username==null) || (password==null)  || !(isUserPermissionExist(permission)) || !(TeamManager.getInstance().isTeamExist(teamName))) {
+                return false;
+            }
             //create user object
-            User newUser = new User(username, password, permission,teamName);
+            User newUser = new User(username, password, permission, teamName);
             //insert new user to team list
-            //TODO: Changed by TAMAR
-            LoginView.teamManager.addMemberToTeam(username, LoginView.teamManager.teams.get(teamName));
+            TeamManager.getInstance().addMemberToTeam(username,  TeamManager.getInstance().teams.get(teamName));
             //insert new user to users HashMap
             users.put(username, newUser);
             //update the user file with new user
@@ -108,21 +117,21 @@ public class UserManager {
                 //user can't remove himself
                 if (!(username.equals(loggedInUser.getUserName()))) {
                     //remove user from the team list
-                    LoginView.teamManager.removeMemberFromTeam(username, LoginView.teamManager.teams.get(users.get(username).getTeamName()));
+                    TeamManager.getInstance().removeMemberFromTeam(username, TeamManager.getInstance().teams.get(users.get(username).getTeamName()));
                     //change the owner of all WI under the username to Unassigned
-                    for (Map.Entry<Integer, WorkItem> entry : MainUserInterface.WIManager.workItems.entrySet()) //adding rows
+                    for (Map.Entry<Integer, WorkItem> entry : WorkItemManager.getInstance().workItems.entrySet()) //adding rows
                         if (entry.getValue().getOwner() != null && entry.getValue().getOwner().equals(users.get(username).getUserName()))
                             entry.getValue().setOwner("Unassigned");
                     //remove user from users HashMap
                     users.remove(username, users.get(username));
                     //update file
                     updateUsersFile();
-                    System.out.println("user removed\n");
+                    System.out.println(username+" user removed\n");
                     return 1;
                 }
                 //user can't remove himself
                 else {
-                    System.out.println("user can't remove himself\n");
+                    System.out.println(username+" user can't remove himself\n");
                     return 4;
                 }
             }
@@ -138,7 +147,6 @@ public class UserManager {
             return 2;
         }
     }
-
 
     public int updateUserPermission(String username, User.PermissionLevel newPermission)
         /*updateUserPermission
@@ -168,22 +176,33 @@ public class UserManager {
           return: 1- user updated
                   2- Action no permitted
                   3- Invalid to edit admin user
+                  4- Team not exist
          */
     {
         if (isActionPermitted()){
             if( !(username.equals("admin"))) {
-                //remove the user from previous team
-                if (TeamManager.getInstance().teams.containsKey(users.get(username).getTeamName()))
-                    LoginView.teamManager.removeMemberFromTeam(username, LoginView.teamManager.teams.get(users.get(username).getTeamName()));
-                 //set user team to new team
-                users.get(username).setTeam(newTeamName);
-                 //insert the user to current list team
-                //TODO:Changed by TAMAR
-                if (TeamManager.getInstance().teams.containsKey(users.get(username).getTeamName()))
-                    TeamManager.getInstance().addMemberToTeam(username, LoginView.teamManager.teams.get(users.get(username).getTeamName()));
-                updateUsersFile();
-                return 1;
-             }
+                //check if new Team exist
+                if (TeamManager.getInstance().teams.containsKey(newTeamName)){
+                    //remove the user from previous team
+                    if (TeamManager.getInstance().teams.containsKey(users.get(username).getTeamName())){
+                        TeamManager.getInstance().removeMemberFromTeam(username, TeamManager.getInstance().teams.get(users.get(username).getTeamName()));
+                        //set user team to new team
+                        users.get(username).setTeam(newTeamName);
+                        //insert the user to current list team
+                        TeamManager.getInstance().addMemberToTeam(username, TeamManager.getInstance().teams.get(users.get(username).getTeamName()));
+                        updateUsersFile();
+                        return 1;
+                    }
+                    else {
+                        System.out.println((users.get(username).getTeamName()) + " team not exist\n");
+                        return 4;
+                    }
+                }
+                else {
+                    System.out.println("new team:"+newTeamName+" not exist\n");
+                    return 4;
+                }
+            }
             else{
                 System.out.println("Invalid to edit admin user\n");
                 return 3;
@@ -192,7 +211,6 @@ public class UserManager {
         else System.out.println("Action no permitted\n");
         return 2;
     }
-
 
     public Integer login(String username,String password)
         /*login
@@ -219,35 +237,43 @@ public class UserManager {
         return (this.loggedInUser.getPermissionLevel()==User.PermissionLevel.manager || this.loggedInUser.getPermissionLevel()==User.PermissionLevel.admin);
     }
 
+    public Boolean isUserPermissionExist(User.PermissionLevel permission){
+        if ((permission==User.PermissionLevel.admin) || (permission==User.PermissionLevel.manager) || (permission==User.PermissionLevel.member))
+            return true;
+        return false;
+    }
+
     public void loadUsersFileToHashMap() {
-        File usersFile = new File(fileAddress);
-        //if the not file empty,need to load users file to HashMap
-        if (usersFile.length() != 0){
+        String line;
+        User u=null;
+        File usersFile = new File(userFileAddress);
+        //if the file not empty,need to load users file to HashMap
+        if (usersFile.length() != 0) {
             // load users file to HashMap
-          try {
-                FileInputStream userFileInputStream = new FileInputStream(fileAddress);
+            try {
+                FileInputStream userFileInputStream = new FileInputStream(userFileAddress);
                 ObjectInputStream userObjectInputStream = new ObjectInputStream(userFileInputStream);
-                User u = (User) userObjectInputStream.readObject();
-                //TODO: in last iter have IOException
-                while (u != null) {
-                    users.put(u.getUserName(),u);
+                u = (User) userObjectInputStream.readObject();
+                while (u!=null){
+                    users.put(u.getUserName(), u);
                     u = (User) userObjectInputStream.readObject();
                 }
+                /*while ((u = (User) userObjectInputStream.readObject())!=null) {
+                    users.put(u.getUserName(), u);
+                    //u = (User) userObjectInputStream.readObject();
+                }*/
                 userObjectInputStream.close();
                 userFileInputStream.close();
-            } catch (IOException e) {
+            } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
                 //System.out.println("failed to load users file to HashMap\n");
-            }catch (ClassNotFoundException c) {
-                    c.printStackTrace();
-                    System.out.println("Class not found");
             }
         }
     }
 
     public void updateUsersFile(){
         try {
-            FileOutputStream userFileOutputStream = new FileOutputStream(fileAddress);
+            FileOutputStream userFileOutputStream = new FileOutputStream(userFileAddress);
             ObjectOutputStream userObjectOutputStream = new ObjectOutputStream(userFileOutputStream);
             for (Map.Entry<String, User> entry : users.entrySet())
                 userObjectOutputStream.writeObject(entry.getValue());
@@ -257,5 +283,11 @@ public class UserManager {
             e.printStackTrace();
             System.out.println("failed to update user file from HashMap\n");
         }
+    }
+
+    public static void setUserFileAddress(String fileAddress)
+    // only change for test purposes
+    {
+        UserManager.userFileAddress = fileAddress;
     }
 }
